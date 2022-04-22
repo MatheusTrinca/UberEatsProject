31ed041fad4f7b814218724a8eb98025041eae86
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-04-22 02:21:56 -0300

    fix modal

--- UberEatsCourier/src/screens/OrdersScreen/index.jsx
+++ UberEatsCourier/src/screens/OrdersScreen/index.jsx
@@ -1,17 +1,19 @@
 import { useRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 
 const OrdersScreen = () => {
   const bottomSheetRef = useRef(null);
 
   return (
-    <View style={{ backgroundColor: 'blue', flex: 1 }}>
-      <Text>Hello World!</Text>
+    <View style={{ backgroundColor: 'lightblue', flex: 1 }}>
+      <BottomSheet ref={bottomSheetRef} snapPoints={['12%', '95%']}>
+        <View style={{ flex: 1 }}>
+          <Text>Hello World!</Text>
+        </View>
+      </BottomSheet>
     </View>
   );
 };
 
 export default OrdersScreen;
-
-const styles = StyleSheet.create({});
